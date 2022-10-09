d34c5c5c86cca2d9537ea28bf440179b343080a5
Sarvesh-stu <s3946533@student.rmit.edu.au>
2022-10-09 19:08:35 +1100

    more comments

--- bin/app.java
+++ bin/app.java
@@ -12,37 +12,43 @@ class app {
 
         // String overview = ManagePackage.GetOverview(); # doesnt work
 
-        System.out.print("Select a package:" + Event.EventPackages());
-        String choice = userInput.nextLine();
-        System.out.print("Approximantly how many people will be attending?");
-        String tempNum = userInput.nextLine();
-            Event.SelectPackage(choice, tempNum);
-            System.out.println("the package has been selected and put on the system with the approximate number of guests as: " + tempNum);
+        System.out.print("Select a package:" + Event.EventPackages());//this is used to display the inofrmation we are asking form the user
+        String choice = userInput.nextLine();//this is used in order to store the users input
+        System.out.print("Approximantly how many people will be attending?");//this is to ask how many people they are expecting at this event
+        String tempNum = userInput.nextLine();//this is to store that number and choice
+            Event.SelectPackage(choice, tempNum);//save the information entered by the user
+            System.out.println("the package has been selected and put on the system with the approximate number of guests as: " + tempNum);//tell the user the state of the sytem
             
-        System.out.print("Select a venue:" + Event.ViewVenue());
+        System.out.print("Select a venue:" + Event.ViewVenue());//this shows the venues
         choice = userInput.nextLine();
-        if (Event.Availability() == true ){
-            System.out.println("The venue is avalible and has been successfully booked");
-        }else{
+
+        boolean chosen = true;
+
+        while (!chosen){
+            if (Event.Availability() == true ){// this is to check weather that event is avalible or not
+                System.out.println("The venue is avalible and has been successfully booked");
+                chosen = true;
+            }else{//if not then the user keeps on trying
 
             System.out.println("Unforunatly the venue is not avalible that day please select another" + Event.ViewVenue());
+            }
         }
         
 
-        System.out.print("Select food:" + Event.ViewFoodMenu());
+        System.out.print("Select food:" + Event.ViewFoodMenu());//this prints out the food menu
         choice = userInput.nextLine();
-        Event.SelectFood(choice);
+        Event.SelectFood(choice);//the system stores the food option chosen
         System.out.println("your food menu has been selected");
 
-        System.out.print("Would you like any optional services? please type \"yes\" or \"no\""  + Event.ViewOptional());
+        System.out.print("Would you like any optional services? please type \"yes\" or \"no\""  + Event.ViewOptional());//this is to make sure the user can pick optional services
         choice = userInput.nextLine();
-        if (choice.compareToIgnoreCase("yes") == 0){
+        if (choice.compareToIgnoreCase("yes") == 0){//this does a check to see if the user is interseted 
             System.out.print("What service would you like?");
             choice = userInput.nextLine();
-            Event.SelectOptionalService(choice);
+            Event.SelectOptionalService(choice);//this is used to store services onto the system
         }
 
-        System.out.println("all your selections have been made the estimated cost of your event is $(placeholder)");
+        System.out.println("all your selections have been made the estimated cost of your event is $(placeholder)");//this prints out the initial cost from the user
 
     }
 }
