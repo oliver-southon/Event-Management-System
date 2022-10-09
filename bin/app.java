e85c315ef6bb0b1d41b7e5728c807a3a9e304dd5
oliver-southon <oliversouthon@gmail.com>
2022-10-09 17:21:13 +1100

    user-input-example

--- bin/app.java
+++ bin/app.java
@@ -1,5 +1,28 @@
 package bin;
 
-public class app {
-    
+import java.util.*;
+
+import bin.eventmanagement.ManagePackage;
+
+class app {
+    public static void app(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        // String overview = ManagePackage.GetOverview(); # doesnt work
+
+        System.out.print("Select a package:\n >1. Family (up to 15 people)\n>2. Party (up to 50 people)\n >3. Grand (up to 150 people)")
+        String choice = userInput.nextLine();
+        if (choice == "1") {
+            ;
+        }
+
+        else if (choice == "2") {
+            ;
+        }
+
+        else if (choice == "3") {
+            ;
+        }
+
+    }
 }
